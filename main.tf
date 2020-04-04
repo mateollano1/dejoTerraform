44c6f95a22cc68e22dbc11f36ba6e92af66d5b27
Mateo Llano <mateo.llano1@gmail.com>
2020-04-04 12:18:56 -0500

    second execise completed

--- main.tf
+++ main.tf
@@ -2,11 +2,95 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "example"{
-    ami = "ami-0e01ce4ee18447327"
-    instance_type = "t2.micro"
+variable "server_port" {
+    description = "the port server will use for http requests"
+    default = 8080
+}
+data "aws_availability_zones" "all" {
 
-    tags = {
-        Name ="terraform-mateoLlano"
+}
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-0fc20dd1da406780b"
+    instance_type = "t2.micro"
+    security_groups = ["${aws_security_group.instance.id}"]
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, world Mateo " > index.html
+                nohup busybox httpd -f -p "${var.server_port}" &
+                EOF
+    lifecycle {
+        create_before_destroy = true
     }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = "${data.aws_availability_zones.all.names}"
+
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+      key = "Name"
+      value = "terraform-mateollano-cluster"
+      propagate_at_launch = true
+  }
+}
+
+
+
+resource "aws_security_group" "instance"{
+    name = "terraform-mateollano"
+
+    ingress {
+        from_port = "${var.server_port}"
+        to_port = "${var.server_port}"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+resource "aws_elb" "example" {
+  name = "terraform-mateollano-cluster"
+  availability_zones = "${data.aws_availability_zones.all.names}"
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+      lb_port = "80"
+      lb_protocol = "http"
+      instance_port = "${var.server_port}"
+      instance_protocol = "http"
+  }
+  health_check {
+      healthy_threshold = 2
+      unhealthy_threshold = 2
+      timeout = 3
+      interval = 30
+      target = "HTTP:${var.server_port}/"
+  }
+}
+resource "aws_security_group" "elb" {
+  name = "terraform-mateollano-elb"
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
